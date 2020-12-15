6a1e15eca17e608900dd75daa91db31768f52cfa
anilesh-k <MY_NAME@example.comanileshk321@gmail.com>
2020-12-15 16:54:15 +0530

    asff

--- emenu/settings.py
+++ emenu/settings.py
@@ -99,6 +99,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_venv = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_venv)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
